4c91ac727866ccbfb75860da96a67da43f2ffd99
GranThed <benjamincanas1@gmail.com>
2020-07-10 04:26:21 -0300

    Updates

---
+++ precache-manifest.a84a151a61e1681b7f008576fbf7a3ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "214680241a86c95de85a4646cb6d7bcd",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "719d73426f824f54e55e",
+    "url": "/quote-machine/static/css/main.b1a99ac0.chunk.css"
+  },
+  {
+    "revision": "ce3136b6329144131904",
+    "url": "/quote-machine/static/js/2.e3889d6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-machine/static/js/2.e3889d6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "719d73426f824f54e55e",
+    "url": "/quote-machine/static/js/main.8ae202a6.chunk.js"
+  },
+  {
+    "revision": "256db52c5a0c9de87c09",
+    "url": "/quote-machine/static/js/runtime-main.6ade76f3.js"
+  }
+]);
